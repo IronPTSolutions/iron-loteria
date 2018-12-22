8a2a27f8327d8f8056648bcf06ce7c9d6fa1dc9a
Mario Quiroga <marioqui135@gmail.com>
2018-12-22 12:55:07 +0100

    Finished

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const hbs = require('hbs');
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const app = express();
+const axios = require('axios');
+const say = require('say');
+
+
 
 
 app.set('views', __dirname + '/views');
@@ -14,6 +18,35 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes go here!
+
+
+
+app.get('/',  (req, res, next) => {
+  res.render('index')
+});  
+
+app.post('/checkNum', (req, res, next) => {
+//  const number =  req.body.number; 
+ const { number } =  req.body; 
+ axios.get(`http://api.elpais.com/ws/LoteriaNavidadPremiados?n=${number}`)
+  .then(response => {
+    const json = JSON.parse(response.data.split('=')[1])
+    say.speak(`${json.premio} EEEEEEEEEEEEEEUUUROOOOOOOOOOOOOS`)
+    console.log(`NÚMERO: ${json.numero}`)
+    console.log(`PREMIO: ${json.premio}`)
+    
+
+    
+      res.render('results', { json: json })
+    
+    
+    
+  })
+})
+
+
+
+
 
 app.listen(PORT, () => {
   console.info(`App listen at ${PORT} port`);
